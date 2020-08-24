f696ba2436400857bee5c5ad028c312fba8f52f9
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-24 00:33:16 -0600

    app5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./Image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://npr.brightspotcdn.com/dims4/default/452ab03/2147483647/strip/true/crop/640x336+0+71/resize/1200x630!/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fkcur%2Ffiles%2F201611%2F5720304856_1c2a7c2142_z.jpg"} />
+      </div>
+    );
+  }
 }
 
 export default App;
